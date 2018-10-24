6041dd81964a0c9f9e3f33baac3a9b65937a3c19
Andrew <ak@arcanite.ru>
2018-10-24 13:32:40 +0300

    last changes

--- ProxySponsorProd/settings.py
+++ ProxySponsorProd/settings.py
@@ -124,15 +124,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Email settings
+# Email settings, place yours
 
 EMAIL_HOST = 'smtp.yandex.ru'
 
 EMAIL_PORT = 465
 
-EMAIL_HOST_USER = 'ak@arcanite.ru'
+EMAIL_HOST_USER = ''
 
-EMAIL_HOST_PASSWORD = 'ASDqwe123'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_USE_SSL = True
 
